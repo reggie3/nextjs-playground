17cc62b0d43f2256a394ba4a42bb4cba557a5eac
Reginald Johnson <reggie3@protonmail.com>
2022-05-12 06:42:32 -0400

    fixing build

---
+++ src/components/HomePageVisualization/Components/HomePageVisualizationContent/HomePageVisualizationContent.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const HomePageVisualizationContent = (props: Props) => {
+  return <group>HomePageVisualizationContent</group>;
+};
+
+export default HomePageVisualizationContent;
---
+++ src/components/HomePageVisualization/Components/HomePageVisualizationContent/index.ts
@@ -0,0 +1,1 @@
+export { default as HomePageVisualizationContent } from "./HomePageVisualizationContent";
