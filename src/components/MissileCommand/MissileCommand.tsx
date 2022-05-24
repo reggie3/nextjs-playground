8aaf07e93a045f87c6ddafcfdc20763530181621
Reginald Johnson <reggie3@protonmail.com>
2022-05-24 16:32:32 -0400

    add orbit controls option

--- src/components/MissileCommand/MissileCommand.tsx
+++ src/components/MissileCommand/MissileCommand.tsx
@@ -3,7 +3,12 @@ import { OrbitControls } from "@react-three/drei";
 import { OrthographicCamera } from "@react-three/drei";
 import { ThreeEvent } from "@react-three/fiber";
 import { Canvas } from "@react-three/fiber";
-import { ForwardRefExoticComponent, RefAttributes, useRef } from "react";
+import {
+  ForwardRefExoticComponent,
+  RefAttributes,
+  useEffect,
+  useRef,
+} from "react";
 import { ReactReduxContext } from "react-redux";
 
 import { MissileCommandContent } from "./components/MissileCommandContent";
@@ -11,13 +16,17 @@ import { GAME_FIELD_HEIGHT, GAME_FIELD_WIDTH } from "./missileCommandGlobals";
 import useMissileCommandControl from "./useMissileCommandControls";
 
 const MissileCommand = () => {
-  const { cameraPos, cameraZoom, orbitControls } = useMissileCommandControl();
+  const {
+    cameraPos,
+    cameraZoom,
+    orbitControls: shouldUseOrbitControls,
+  } = useMissileCommandControl();
   type MissileCommandContentHandle = React.ElementRef<
     typeof MissileCommandContent
   >;
 
   const contentRef = useRef<MissileCommandContentHandle>(null);
-
+  const cameraRef = useRef<THREE.Camera>();
   // Need this to make Redux work inside the canvas
   const ReduxProvider = useContextBridge(ReactReduxContext);
 
@@ -28,11 +37,19 @@ const MissileCommand = () => {
     }
   };
 
+  useEffect(() => {
+    if (!shouldUseOrbitControls && cameraRef.current) {
+      cameraRef.current.position.set(0, 5.5, 5);
+      cameraRef.current.lookAt(0, 5.5, 5);
+    }
+  }, [shouldUseOrbitControls]);
+
   return (
     <Canvas dpr={[1, 2]} shadows onClick={onClickCanvas}>
       <ReduxProvider>
         <axesHelper args={[10]} />
         <OrthographicCamera
+          ref={cameraRef}
           args={[
             -GAME_FIELD_WIDTH / 2,
             GAME_FIELD_WIDTH / 2,
@@ -45,7 +62,7 @@ const MissileCommand = () => {
         />
         <MissileCommandContent ref={contentRef} />
       </ReduxProvider>
-      {true && <OrbitControls />}
+      {shouldUseOrbitControls && <OrbitControls />}
     </Canvas>
   );
 };
--- src/components/MissileCommand/useMissileCommandControls.ts
+++ src/components/MissileCommand/useMissileCommandControls.ts
@@ -3,16 +3,16 @@ import { CAMERA_Y_POS } from "./missileCommandGlobals";
 
 const useMissileCommandControl = () => {
   // @ts-ignore
-  const [{ cameraPos, cameraZoom, setCameraControls }, set] = useControls(
+  const [{ cameraPos, cameraZoom, orbitControls }, set] = useControls(
     "Camera Controls",
     // @ts-ignore
     () => ({
       cameraZoom: { label: "zoom", value: 55, min: 10, max: 100, step: 1 },
       cameraPos: { label: "Position", x: 0, y: CAMERA_Y_POS, z: 5 },
-      orbitControls: { label: "Orbit Controls", value: false },
+      orbitControls: { label: "UseOrbit Controls", value: false },
     })
   );
-  return { cameraPos, cameraZoom, setCameraControls: set };
+  return { cameraPos, cameraZoom, setCameraControls: set, orbitControls };
 };
 
 export default useMissileCommandControl;
